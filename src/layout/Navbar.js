09e14f79fddfb9cb10da4747bf56c8866af06fc9
unknown <vladislavcukanovfree@gmail.com>
2022-09-02 18:57:08 +0300

    navbar working

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Logo from '../assets/img/SRG logo/SRG logo.png'
 
 const Navbar = () => {
     return(
-        <div>
-            asdf
+        <div className="w-full shadow-lg h-20">
+            <img src={Logo} alt="logo" className="absolute top-3.5 left-8" />
+            <input type="text" className="absolute left-[477px] top-3.5 border border-slate-400 w-2/5 h-11 rounded-lg" placeholder="Search items, collections and accounts"/>
+
         </div>
     )
 }
